c421ab40bf033ba1bb8e5af418c8e6b96c6731b8
Christoph Wille <christoph.wille@gmail.com>
2021-04-29 09:46:11 +0200

    Add simple task executable (to be packaged and deployed) plus a job task dispatcher

---
+++ src/BatchTaskApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using System.Text;
+
+namespace BatchTaskApp
+{
+	class Program
+	{
+		static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+		[Option("-b|--base64", Description = "Base64 input to write to file")]
+		public string Base64 { get; } = "SGVsbG8gV29ybGQh"; // "Hello World!"
+
+		const string TargetFilenameByConvention = "target.txt";
+
+		private async Task<int> OnExecuteAsync(CommandLineApplication app, CancellationToken cancellationToken = default)
+		{
+			byte[] data = Convert.FromBase64String(Base64);
+			string decodedString = Encoding.UTF8.GetString(data);
+
+			using (var fs = File.OpenWrite(TargetFilenameByConvention))
+			using (var sw = new StreamWriter(fs))
+			{
+				await sw.WriteAsync(decodedString);
+				await sw.FlushAsync();
+			}
+
+			return 0;
+		}
+	}
+}
---
+++ src/Dispatch/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Threading;
+using System.Threading.Tasks;
+using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Azure.Batch;
+using System.Collections.Generic;
+using Microsoft.Azure.Batch.Auth;
+
+namespace Dispatch
+{
+	class Program
+	{
+		static Task<int> Main(string[] args) => CommandLineApplication.ExecuteAsync<Program>(args);
+
+		[Option("-s|--serviceurl", Description = "Service URL")]
+		[Required]
+		public string BatchServiceUrl { get; } = "https://cwiazbatchtest.westeurope.batch.azure.com";
+
+		[Option("-a|--accountname", Description = "Account name")]
+		[Required]
+		public string BatchAccountName { get; } = "cwiazbatchtest";
+
+		[Option("-k|--accountkey", Description = "Account key")]
+		[Required]
+		public string BatchAccountKey { get; }
+
+		const string BatchAppNameByConvention = "BatchTaskApp";
+		const string BatchAppExecutableByConvention = "batchtaskapp.exe";
+		const string BatchJobNameByConvention = "singlejob";
+
+		private async Task<int> OnExecuteAsync(CommandLineApplication app, CancellationToken cancellationToken = default)
+		{
+			var _batchCredentials = new BatchSharedKeyCredentials(BatchServiceUrl, BatchAccountName, BatchAccountKey);
+			using var batchClient = BatchClient.Open(_batchCredentials);
+
+			string taskId = Guid.NewGuid().ToString();
+			string cmd = $"cmd /c %AZ_BATCH_APP_PACKAGE_{BatchAppNameByConvention}%\\{BatchAppExecutableByConvention} -b SGkgQ2hyaXM=";
+
+			// Intentionally not collecting any outputs (simplicity)
+			CloudTask taskToCreate = new CloudTask(taskId, cmd)
+			{
+				ApplicationPackageReferences = new List<ApplicationPackageReference>
+				{
+					new ApplicationPackageReference()
+					{
+						ApplicationId = BatchAppNameByConvention
+					}
+				}
+			};
+
+			await batchClient.JobOperations.AddTaskAsync(BatchJobNameByConvention, taskToCreate);
+			return 0;
+		}
+	}
+}
